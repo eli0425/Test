f3252f4d1bafbf2df177ac07f27e84f311b96bc5
eli <eli@testo.net.cn>
2017-04-28 09:46:14 +0800

    [Elin Li] Base code miss file upload.

---
+++ mfcapp/mfcapp.h
@@ -0,0 +1,32 @@
+// mfcapp.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CmfcappApp:
+// 有关此类的实现，请参阅 mfcapp.cpp
+//
+
+class CmfcappApp : public CWinApp
+{
+public:
+	CmfcappApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+	afx_msg void OnFilePrintSetup();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmfcappApp theApp;
